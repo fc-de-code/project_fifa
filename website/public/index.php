2bc7dfcec655af3c576b738eae79b10c6190eabc
Jean pierre <d210612@edu.rocwb.nl>
2017-05-04 11:57:05 +0200

    Auto stash before merge of "master" and "origin/master"

--- website/public/index.php
+++ website/public/index.php
@@ -4,7 +4,9 @@
     <div class="main-content">
       <div class="hero">
         <div class="wrapper">
-          <h1>fifa dev edition</h1>
+          <h1 class="text-align">RADIUS</h1>
+          <img src="img/LogoTest.png" alt="logo-test">
+          <h1>FIFA</h1>
         </div>
       </div>
       <nav>
@@ -36,14 +38,51 @@
       </div>
       <div class="introwebsite">
         <div class="wrapper">
-
+          <h2>Introductie</h2>
+          <div class="content">
+            <p>
+              hallo en welkom op de radius fifa site.
+              deze site is gemaakt voor het berekenen en bijhouden van wedstrijden voor het voetballen, hierbij is ook een applicatie te downloaden om te gokken op de teams die spelen. op de site wordend de wedstrijden gemaakt en de scores bij gehouden, en in de applicatie kan je gokken op welk team er volgens jou gaat winnen.
+            </p>
+            <p>
+              Op deze website kan je via het navigatie menu naar verschillen web pagina's gaan.
+              Op dit moment zijn wij op de home pagina, hier vindt je de introductie die u nu aan het lezen ben maar ook de totaal score van alle team die zich geregistreert hebben.
+              Op de finale pagina vind u de wedstrijden die gespeelt gaan worden.
+              Op de resultaaten pagina vind u de uitslagen per wedstrijden die er gespeeld zijn.
+              Op de teams pagin vind u alle teams die op deze site geregistreert zijn.
+            </p>
+          </div>
         </div>
       </div>
-      <div class="updates">
+      <div class="team-score">
         <div class="wrapper">
-
+          <h2>Teams Score</h2>
+          <div class="content">
+            <div class="team-list">
+              <ul>
+                <li>team1</li>
+                <li>team2</li>
+                <li>team3</li>
+                <li>team4</li>
+                <li>team5</li>
+                <li>team6</li>
+                <li>team7</li>
+                <li>team8</li>
+                <li>team9</li>
+                <li>team10</li>
+                <li>team11</li>
+                <li>team12</li>
+                <li>team13</li>
+                <li>team14</li>
+                <li>team15</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
+      <footer>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </footer>
     </div>
 
     <script src="js/loginpopup.js"></script>
--- website/public/templates/header.php
+++ website/public/templates/header.php
@@ -6,5 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Project Fifa</title>
     <link rel="stylesheet" href="css/normal.css">
+    <link href="https://fonts.googleapis.com/css?family=Baloo" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Baloo|Shrikhand" rel="stylesheet">
 </head>
 <body>
