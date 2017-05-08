04eb1580c91c0a88ad8ecd85301967123b5cf0e4
Jean pierre <d210612@edu.rocwb.nl>
2017-05-08 16:10:03 +0200

    website--database-connection
    added datebase connection files

--- website/app/db-connection.php
+++ website/app/db-connection.php
@@ -1,12 +1,13 @@
 <?php
-$server = 'localhost';
-$username = 'root';
-$password = 'welkom1';
-$database = 'todo';
+namespace App;
+use PDO;
 
-try{
-  $conn = new PDO("mysql:host=$server;dbname=$database", $username, $password);
-}catch(PDOException $e){
-  die("connection failed:" . $e->getMessage());
+require_once("../config/Database-Config.php");
+
+function Connect()
+{
+    $dbc = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
+    $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbc;
 }
 ?>
---
+++ website/config/database-config.php
@@ -0,0 +1,6 @@
+<?php
+DEFINE ("DB_USER","root");
+DEFINE ("DB_PASSWORD","welkom1");
+DEFINE ("DB_HOST","localhost");
+DEFINE ("DB_NAME","project_fifa");
+?>
