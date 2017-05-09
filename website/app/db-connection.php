57f04626e9414eb656e06dbea96db1f4802ced0f
Jean pierre <d210612@edu.rocwb.nl>
2017-05-09 13:31:45 +0200

    website-PHP-database
    --added database connction OOP-PDO style
    --removed database-config.php

--- website/app/db-connection.php
+++ website/app/db-connection.php
@@ -1,13 +1,32 @@
 <?php
-namespace App;
-use PDO;
-
-require_once("../config/Database-Config.php");
-
-function Connect()
+class connection
 {
-    $dbc = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
-    $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $dbc;
+  protected $connection_db_link;
+  public $db_user = 'root';
+  public $db_host = 'localhost';
+  public $db_name = 'project_fifa';
+  public $db_pass = '';
+
+  function db_connect()
+  {
+    try {
+      $this->connection_db_link = new PDO("mysql:host=$this->db_host;$this->db_name",$this->db_user,$this->db_pass);
+      $this->connection_db_link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $this->connection_db_link;
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+
+  }
+  function display_connection()
+  {
+    if ($this->connection_db_link == true)
+    {
+      echo 'connection succes';
+    }
+  }
 }
-?>
+$users = new connection;
+$users->db_connect();
+$users->display_connection();
+ ?>
--- website/config/database-config.php
+++
@@ -1,6 +0,0 @@
-<?php
-DEFINE ("DB_USER","root");
-DEFINE ("DB_PASSWORD","welkom1");
-DEFINE ("DB_HOST","localhost");
-DEFINE ("DB_NAME","project_fifa");
-?>
